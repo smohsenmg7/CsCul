c70fc33e6772376280c3fca55b543ab97c906230
Mohsen <SMOHSENMG7@GMAIL.COM>
2018-09-02 17:24:17 +0430

    fix Equal button

--- app/src/main/java/com/example/amk/cscul/MainActivity.java
+++ app/src/main/java/com/example/amk/cscul/MainActivity.java
@@ -1,24 +1,24 @@
 /**
  * issues *
- *
- *      1 - if ( TempVariable == "" )
- *          prevent from axcecuting operator !!!
- *
- *      2 - Delete  Button 
+ * <p>
+ * 1 - if ( TempVariable == "" )
+ * prevent from axcecuting operator !!!
+ * <p>
+ * 2 - Delete  Button
  */
 
 package com.example.amk.cscul;
 
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
     // when an variable get completed will save in this ArrayList
     public static ArrayList<String> variables = new ArrayList<>();
     // operator commands will save in this ArrayList ( usage : Equal method )
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     boolean EqEffect = false;
     // this variable get updated till user input an operator
     String TempVariable = "";
+    TextView editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,24 +35,145 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         // hiding status bar and action bar
-        if(Build.VERSION.SDK_INT > 16)
-        {
+        if (Build.VERSION.SDK_INT > 16) {
             View decorView = getWindow().getDecorView();
             int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
             decorView.setSystemUiVisibility(uiOptions);
             getSupportActionBar().hide();
         }
-
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edit_text);
+        numbersListener();
+        operationsListener();
+
+        TextView equal = findViewById(R.id.equal);
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                variables.add(TempVariable);
+                TempVariable = "";
+
+                for (int i = 0; i < operations.size(); i++) {
+                    if (variables.size() == 0)
+                        editText.setText("");
+                    if (variables.size() == 1)
+                        editText.setText(variables.get(0));
+                    String ops = operations.get(i);
+
+                    switch (ops) {
+                        case "multiple":
+                            Refactor(operations.indexOf(ops), Multiple(operations.indexOf(ops)));
+                            i--;
+                            break;
+                        case "division":
+                            Refactor(operations.indexOf(ops), Division(operations.indexOf(ops)));
+                            i--;
+                            break;
+                    }
+                }
+                for (int i = 0; i < operations.size(); i++) {
+                    if (variables.size() == 0)
+                        editText.setText("");
+                    if (variables.size() == 1)
+                        editText.setText(variables.get(0));
+                    String ops = operations.get(i);
+
+                    switch (ops) {
+                        case "plus":
+                            Refactor(operations.indexOf(ops), Plus(operations.indexOf(ops)));
+                            i--;
+                            break;
+                        case "minus":
+                            Refactor(operations.indexOf(ops), Minus(operations.indexOf(ops)));
+                            i--;
+                            break;
+                    }
+                }
+
+
+                editText.setText(variables.get(0));
+            }
+        });
+
+    }
+
+    private void operationsListener() {
+        TextView division = findViewById(R.id.division_button);
+        division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "÷");
+                variables.add(TempVariable);
+                operations.add("division");
+                TempVariable = "";
+            }
+        });
+        /**
+         *      Multiple    (   ×   )
+         */
+        TextView multiple = findViewById(R.id.multiple_button);
+        multiple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "×");
+                variables.add(TempVariable);
+                operations.add("multiple");
+                TempVariable = "";
+            }
+        });
+        /**
+         *      Minus   (   -   )
+         */
+        TextView minus = findViewById(R.id.minus);
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "-");
+                variables.add(TempVariable);
+                operations.add("minus");
+                TempVariable = "";
+            }
+        });
 
         /**
-         *      EditText
+         *      Plus    (   +   )
          */
-        final TextView editText = findViewById(R.id.edit_text);
-
+        TextView plus = findViewById(R.id.plus);
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + "+");
+                variables.add(TempVariable);
+                operations.add("plus");
+                TempVariable = "";
+            }
+        });
         /**
-         *      Button 0
+         *      Button Clean
          */
+        TextView clean = findViewById(R.id.clean_button);
+        clean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                variables.clear();
+                operations.clear();
+                editText.setText("");
+            }
+        });
+    }
+
+    private void numbersListener() {
+        /**
+         *      Point   (   .   )
+         */
+        TextView point = findViewById(R.id.point_button);
+        point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText(editText.getText() + ".");
+                TempVariable += ".";
+            }
+        });
         TextView zero = findViewById(R.id.zero_button);
         zero.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,7 +226,7 @@ public class MainActivity extends AppCompatActivity {
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText()+"4");
+                editText.setText(editText.getText() + "4");
                 TempVariable += "4";
             }
         });
@@ -140,7 +262,6 @@ public class MainActivity extends AppCompatActivity {
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                numberAfterEqual(EqEffect, editText);
                 editText.setText(editText.getText() + "7");
                 TempVariable += "7";
             }
@@ -153,7 +274,7 @@ public class MainActivity extends AppCompatActivity {
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editText.setText(editText.getText()+"8");
+                editText.setText(editText.getText() + "8");
                 TempVariable += "8";
             }
         });
@@ -170,264 +291,33 @@ public class MainActivity extends AppCompatActivity {
                 TempVariable += "9";
             }
         });
-
-        /**
-         *      Division    (   ÷   )
-         */
-        TextView division = findViewById(R.id.division_button);
-        division.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                operatorAfterEqual(EqEffect);
-                editText.setText(editText.getText() + "÷");
-                variables.add(TempVariable);
-                operations.add("division");
-                TempVariable = "";
-            }
-        });
-
-        /**
-         *      Multiple    (   ×   )
-         */
-        TextView multiple = findViewById(R.id.multiple_button);
-        multiple.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                editText.setText(editText.getText()+"×");
-                variables.add(TempVariable);
-                operations.add("multiple");
-                TempVariable = "";
-            }
-        });
-
-        /**
-         *      Minus   (   -   )
-         */
-        TextView minus = findViewById(R.id.minus);
-        minus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                editText.setText(editText.getText() + "-");
-                variables.add(TempVariable);
-                operations.add("minus");
-                TempVariable = "";
-            }
-        });
-
-        /**
-         *      Plus    (   +   )
-         */
-        TextView plus = findViewById(R.id.plus);
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                editText.setText(editText.getText() + "+");
-                variables.add(TempVariable);
-                operations.add("plus");
-                TempVariable = "";
-            }
-        });
-
-        /**
-         *      Point   (   .   )
-         */
-        TextView point = findViewById(R.id.point_button);
-        point.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                editText.setText(editText.getText()+".");
-                TempVariable += ".";
-            }
-        });
-
-        /**
-         *      Button Clean
-         */
-        TextView clean = findViewById(R.id.clean_button);
-        clean.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                variables.clear();
-                operations.clear();
-                editText.setText("");
-            }
-        });
-
-        /**
-         *      Button mc
-         */
-        TextView mc = findViewById(R.id.mc_button);
-        mc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
-        /**
-         *      percent (   %   )
-         */
-        TextView percent = findViewById(R.id.percent_sign);
-        percent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "percent ...", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        /**
-         *      mPlus   (   m+  )
-         */
-        TextView mPlus = findViewById(R.id.mPlus_button);
-        mPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "mPlus ...", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        /**
-         *      mMinus   (   m-  )
-         */
-        TextView mMinus = findViewById(R.id.mMinus_button);
-        mMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "mMinus ...", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        /**
-         *      mr  (   mr  )
-         */
-        TextView mr = findViewById(R.id.mr);
-        mr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-
-        /**
-         *      Delete  (   Warning : Under Construction  :)  )
-         */
-        TextView delete = findViewById(R.id.del);
-        delete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "delete ...", Toast.LENGTH_SHORT).show();
-//                Editable text = editText.getText();
-//                String sText= editText.getText().toString();
-//                //char LC = sText.charAt(sText.length()-1);
-//                String LC =sText.substring(sText.length()-1);
-//                boolean importent ;
-//                if(TempVariable != null){
-//                    TempVariable = TempVariable.substring(0,TempVariable.length()-1);
-//                    editText.setText(editText.getText().subSequence(0,text.length()-1));
-//                }
-//                //if(LC == '+' || LC == '-' || LC == '×' || LC == '÷')
-//                if(LC.equals("+") || LC.equals("-") || LC.equals("×") || LC.equals("÷"))
-//                {
-//                    editText.setText(editText.getText().subSequence(0,text.length()-1));
-//                    importent = true ;
-//                }
-//                if(TempVariable == null || TempVariable.equals("")){
-//                    String MainString = variables.get(variables.size()-1);
-//                    String EditedString = MainString.substring(0,MainString.length()-1);
-//                    variables.add(variables.size()-1,EditedString);
-//                }
-            }
-        });
-
-        /**
-         *      Equal   (   =   )
-         */
-        TextView equal = findViewById(R.id.equal);
-        equal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                variables.add(TempVariable);
-                TempVariable = "";
-                for (String ops: operations){
-                    if(variables.size() == 0)
-                        editText.setText("");
-                    if(variables.size() == 1)
-                        editText.setText(variables.get(0));
-                    switch (ops){
-                        case "plus" :
-                            Refactor(Plus());
-                            break;
-                        case "minus" :
-                            Refactor(Minus());
-                            break;
-                        case "multiple" :
-                            Refactor(Multiple());
-                            break;
-                        case "division" :
-                            Refactor(Division());
-                            break;
-                        default:
-                    }
-                    EqEffect = true;
-                }
-                editText.setText(variables.get(0));
-            }
-        });
-
     }
 
-    // Operation Methods Declaration    plus    minus   multiple    division
 
-    public String Plus() {
-        if (variables.get(1) == null)
-            return variables.get(0);
-        double result = Double.parseDouble(variables.get(0)) + Double.parseDouble(variables.get(1));
-        return "" + result ;
+    public String Plus(int indexOfOperation) {
+        double result = Double.parseDouble(variables.get(indexOfOperation)) + Double.parseDouble(variables.get(indexOfOperation + 1));
+        return "" + result;
     }
 
-    public String Minus() {
-        if (variables.get(1) == null)
-            return variables.get(0);
-        double result = Double.parseDouble(variables.get(0)) - Double.parseDouble(variables.get(1));
-        return "" + result ;
+    public String Minus(int indexOfOperation) {
+        double result = Double.parseDouble(variables.get(indexOfOperation)) - Double.parseDouble(variables.get(indexOfOperation + 1));
+        return "" + result;
     }
 
-    public String Multiple() {
-        if (variables.get(1) == null)
-            return variables.get(0);
-        double result = Double.parseDouble(variables.get(0)) * Double.parseDouble(variables.get(1));
-        return "" + result ;
+    public String Multiple(int indexOfOperation) {
+        double result = Double.parseDouble(variables.get(indexOfOperation)) * Double.parseDouble(variables.get(indexOfOperation + 1));
+        return "" + result;
     }
 
-    public String Division() {
-        if (variables.get(1) == null)
-            return variables.get(0);
-        double result = Double.parseDouble(variables.get(0)) / Double.parseDouble(variables.get(1));
-        return "" + result ;
+    public String Division(int indexOfOparation) {
+        double result = Double.parseDouble(variables.get(indexOfOparation)) / Double.parseDouble(variables.get(indexOfOparation + 1));
+        return "" + result;
     }
 
-    //  After Excecuting each operation Refactor method will place Result in index 0 of Variables ArrayList
-    // and
-    // will delete Second variable in Variables ArrayList
-    public void Refactor(String result){
-        variables.set(0,result);
-        variables.remove(1);
-    }
-
-    // reaction for number button after Equal operation
-    public void numberAfterEqual(boolean key, TextView editText){
-        if(key){
-            editText.setText("");
-            variables.clear();
-            operations.clear();
-            this.EqEffect = false;
-        }
-    }
-
-    //  reaction for operation button after Equal operation
-    public void operatorAfterEqual(boolean key){
-        if(key){
-            operations.clear();
-            this.EqEffect = false;
-        }
+    public void Refactor(int indexOfOparation, String result) {
+        variables.set(indexOfOparation, result);
+        variables.remove(indexOfOparation + 1);
+        operations.remove(indexOfOparation);
     }
 
 }
